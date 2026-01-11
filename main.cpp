9f33bcf80df7cddb99a2d0eb42673de6a82e0ad4
Marc Neumann <nema1030@h-ka.de>
2026-01-11 12:37:26 +0100

    CDMA_RX init

--- main.cpp
+++ main.cpp
@@ -1,43 +1,8 @@
-#include "pdl_header.h"
 #include "global.h"
-#include "utils.h"
-#include <math.h>
 
-// --- CDMA CONFIG ---
-#define SF 6
-const float CODE_A[SF] = { 1.0f,  1.0f, -1.0f,  1.0f, -1.0f, -1.0f};
-const float CODE_B[SF] = { 1.0f, -1.0f,  1.0f,  1.0f, -1.0f,  1.0f};
-const float* MY_CODE = CODE_A;                
 
-// --- CFAR CONFIG ---
-// CFAR_SCALE: Wie viel mal größer als das "Rauschen" muss der Peak sein?
-// 3.0 bis 5.0 sind gute Startwerte für CDMA.
-// Wenn er nicht lockt -> Kleiner machen (z.B. 2.5)
-// Wenn er falsch lockt -> Größer machen (z.B. 6.0)
-#define CFAR_SCALE 4.0f     
 
-// CFAR_ALPHA: Wie schnell passt sich der Threshold an die Lautstärke an?
-// 0.01 = Langsam & Stabil, 0.1 = Schnell
-#define CFAR_ALPHA 0.01f
-
-enum ReceiverState {
-    STATE_SEARCHING, 
-    STATE_LOCKED     
-};
-
-ReceiverState current_state = STATE_SEARCHING;
-
-// Variablen
-float search_window[SF] = {0}; 
-int chip_index = 0;            
-float accumulator = 0.0f;      
-float current_audio_out = 0.0f;
-
-// CFAR State Variable
-float average_noise_level = 0.1f; // Startwert (nicht 0, um Division durch 0 zu vermeiden)
-//----------------------------------------------------------------------------------------------------------------
-
-// using the circular_buffer to verify whether the hardware setup is working correctly
+// using the hello_world_circ_buffer to verify whether the hardware setup is working correctly
 CircularBuffer rx_buffer;
 CircularBuffer tx_buffer;
 
@@ -53,18 +18,31 @@ int16_t right_out[BLOCK_SIZE];
 
 int main()
 {
-    // 1. Hardware starten
+    // initialze whole platform, does not start DMA
     init_platform(115200, hz32000, line_in);
+
+    // use debug_printf() to send data to a Serial Monitor
     debug_printf("%s, %s\n", __DATE__, __TIME__);
+
+    // function calls surrounded by IF_DEBUG() will be removed when building a Release
     IF_DEBUG(debug_printf("Hello World!\n"));
+
+    // init test pin P10 to LOW; can be found on the board as part of the connector CN10, Pin is labelled as A3
     gpio_set(TEST_PIN, LOW);
+
+    // initialize circular buffers
     rx_buffer.init();
     tx_buffer.init();
+
     memset(in, 0, sizeof(in));
     memset(out, 0, sizeof(out));
+
+
+    // start I2S, call just before your main loop
+    // this command starts the DMA, which will begin transferring data to and from the rx_buffer and tx_buffer
     platform_start();
 
-while(true)
+    while(true)
     {
         // step 1: read block of samples from input buffer, data is copied from rx_buffer to in
         while(!rx_buffer.read(in));
@@ -101,8 +79,14 @@ while(true)
 
     // fail-safe, never return from main on a microcontroller
     fatal_error();
+
     return 0;
 }
+
+
+
+// the following functions are called, when the DMA has finished transferring one block of samples and needs a new memory address to write/read to/from
+
 // prototype defined in platform.h
 // get new memory address to read new data to send it to DAC
 uint32_t* get_new_tx_buffer_ptr()
